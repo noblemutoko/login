ed64731e149593f271af2157a58c975eaa45dbd0
Noble Mutoko <mutokon9@hotmail.com>
2017-04-04 14:07:52 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="css/style.css"></link>
+  <link rel="stylesheet" type="text/css" href="css/font-awesome.css"></link>
+</head>
+<body>
+  <div class="container">
+    <img src="img/men.png">
+    <form>
+      <div class="form-input">
+        <input type="email" name="email" placeholder="Email">
+      </div>
+      <div class="form-input">
+        <input type="password" name="password" placeholder="Password">
+      </div>
+        <input type="submit" name="submit" value="Submit" class="btn-login"><br/>
+        <a href="#">Forget password?</a>
+    </form>
+  </div>
+</body>
+</html>
